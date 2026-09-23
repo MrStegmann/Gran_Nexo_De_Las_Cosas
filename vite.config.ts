2f274755d702ba0e367de062f5df0cd744bc3097
MrStegmann <pjstegmann@outlook.es>
2026-09-23 16:17:30 +0200

    Feature: Add MFE configuration and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+  // @ts-expect-error: vite-plugin-federation types are incompatible with module: nodenext
+  federation({
+    name: "gran-nexo-de-las-cosas",
+    filename: "remoteEntry.js",
+    exposes: {
+      "./GranNexoDeLasCosas": "./src/App.tsx",
+    },
+    shared: ["react", "react-dom"],
+  }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
